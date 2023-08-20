ca313871241d2df9a433c41bb460899dea0de5a1
Maor <maorsa9@gmail.com>
2023-08-20 18:17:08 +0300

    Refactoring The Code with seprate File

---
+++ controllers/userController.js
@@ -0,0 +1,32 @@
+exports.getAllUsers = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'Not Defined',
+  });
+};
+
+exports.getUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'Not Defined',
+  });
+};
+exports.createUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'Not Defined',
+  });
+};
+exports.updateUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'Not Defined',
+  });
+};
+
+exports.deleteUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'Not Defined',
+  });
+};
