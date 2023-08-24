32838362fed96b5c5d9e4fdedf3709a30a93d40e
Maor <maorsa9@gmail.com>
2023-08-24 13:28:54 +0300

    Added some Moudle Middleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -74,10 +74,10 @@ const tourSchema = new mongoose.Schema(
       select: false,
     },
     startDates: [Date],
-    // secretTour: {
-    //   type: Boolean,
-    //   default: false,
-    // },
+    secretTour: {
+      type: Boolean,
+      default: false,
+    },
   },
   {
     toJSON: { virtuals: true },
@@ -107,17 +107,17 @@ tourSchema.pre('save', function (next) {
 
 // QUERY MIDDLEWARE
 // tourSchema.pre('find', function(next) {
-// tourSchema.pre(/^find/, function (next) {
-//   this.find({ secretTour: { $ne: true } });
+tourSchema.pre(/^find/, function (next) {
+  this.find({ secretTour: { $ne: true } });
 
-//   this.start = Date.now();
-//   next();
-// });
+  this.start = Date.now();
+  next();
+});
 
-// tourSchema.post(/^find/, function (docs, next) {
-//   console.log(`Query took ${Date.now() - this.start} milliseconds!`);
-//   next();
-// });
+tourSchema.post(/^find/, function (docs, next) {
+  console.log(`Query took ${Date.now() - this.start} milliseconds!`);
+  next();
+});
 
 // AGGREGATION MIDDLEWARE
 // tourSchema.pre('aggregate', function (next) {
