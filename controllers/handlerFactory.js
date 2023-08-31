2c23fba868faca1c0b6a98bcb5168975318fb4e8
Maor <maorsa9@gmail.com>
2023-08-31 12:22:41 +0300

    Refctoring the code with use handlerFactory for all the diffrent Models to prevent duplicate code

---
+++ controllers/handlerFactory.js
@@ -0,0 +1,101 @@
+const AppError = require('../utils/appError');
+const catchAsync = require('../utils/catchAsync');
+const APIfeatures = require('./../utils/apiFeatures');
+
+exports.deleteOne = (Model) =>
+  catchAsync(async (req, res, next) => {
+    const doc = await Model.findByIdAndDelete(req.params.id);
+
+    if (!doc) {
+      return next(new AppError('No document found with that ID', 404));
+    }
+
+    res.status(204).json({
+      status: 'success',
+      data: null,
+    });
+  });
+
+exports.UpdateOne = (Model) =>
+  catchAsync(async (req, res, next) => {
+    try {
+      const doc = await Model.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true,
+      });
+
+      if (!doc) {
+        return next(new AppError('No document found with that ID', 404));
+      }
+
+      res.status(200).json({
+        status: 'success',
+        data: {
+          data: doc,
+        },
+      });
+    } catch (err) {
+      res.status(404)({
+        status: 'fail',
+        message: err,
+      });
+    }
+  });
+
+exports.CreateOne = (Model) =>
+  catchAsync(async (req, res, next) => {
+    const doc = await Model.create(req.body);
+
+    res.status(201).json({
+      status: 'success',
+      data: {
+        data: doc,
+      },
+    });
+  });
+
+exports.getOne = (Model, popOptions) =>
+  catchAsync(async (req, res, next) => {
+    let query = Model.findById(req.params.id);
+    if (popOptions) query = query.populate(popOptions);
+    const doc = await query;
+
+    // doc.findOne({_id: req.parmas.id})
+
+    if (!doc) {
+      return next(new AppError('No document found with that ID', 404));
+    }
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        data: doc,
+      },
+    });
+  });
+
+exports.getAll = (Model) =>
+  catchAsync(async (req, res, next) => {
+    // console.log(req.query);
+
+    // To allow for nested Get reviews on tour
+    let filter = {};
+    if (req.params.tourId) filter = { tour: req.params.tourId };
+
+    //EXCUTE QUERY
+    const features = new APIfeatures(Model.find(filter), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+    const doc = await features.query;
+
+    //SEND RESPONSE
+    res.status(200).json({
+      status: 'success',
+      results: doc.length,
+      data: {
+        data: doc,
+      },
+    });
+  });
